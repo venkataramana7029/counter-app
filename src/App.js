ff721efa1f51c103526590a7a5ad218eb2ff288c
venkataramana7029 <109533359+venkataramana7029@users.noreply.github.com>
2023-01-19 23:21:58 +0530

    counerApplication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [count,setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container my-5'>
+      <div className='card m-4 border' >
+        <div className='card-header text-center'>
+          <h1>Counter-App</h1>
+
+        </div>
+
+        <div className='card-body'>
+
+
+          <p className='m-4 text-center'>Count : &nbsp; {count}  &nbsp; times</p>
+
+
+          <div className='m-5 text-center'>
+            <button className='mx-2 px-2 btn btn-success' onClick={()=> setCount(count +1)} >Increment</button>
+            <button className='mx-2 px-2 btn btn-danger' onClick={()=> setCount(count -1)}>Decrement</button>
+            <button className='mx-2 px-2 btn btn-info' onClick={()=> setCount(0)}>Reset</button>
+            
+          </div>
+
+        </div>
+
+      </div>
+    
     </div>
   );
 }
